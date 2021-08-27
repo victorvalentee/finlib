5d6dbf6419230f71fa6ad195af387f843a40c983
Victor Valente <victorvalentee@gmail.com>
2021-08-26 22:51:42 -0300

    Started bars module.

---
+++ finlib/bars.py
@@ -0,0 +1,40 @@
+class TimeBars:
+    def __init__(self) -> None:
+        print('Here it goes!')
+
+class TickBars:
+    def __init__(self) -> None:
+        print('Here it goes!')
+
+class VolumeBars:
+    def __init__(self) -> None:
+        print('Here it goes!')
+
+class DollarBars:
+    def __init__(self) -> None:
+        print('Here it goes!')
+
+class TickImbalanceBars:
+    def __init__(self) -> None:
+        print('Here it goes!')
+
+class VolumeImbalanceBars:
+    def __init__(self) -> None:
+        print('Here it goes!')
+
+class DollarImbalanceBars:
+    def __init__(self) -> None:
+        print('Here it goes!')
+
+class TickRunsBars:
+    def __init__(self) -> None:
+        print('Here it goes!')
+
+class VolumeRunsBars:
+    def __init__(self) -> None:
+        print('Here it goes!')
+
+class DollarRunsBars:
+    def __init__(self) -> None:
+        print('Here it goes!')
+
